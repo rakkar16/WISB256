a73f9cce483d8cfef71b6623aad2568759188390
rakkar16 <dicospel@hotmail.com>
2015-05-21 14:38:41 +0000

    findAllRoots added

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -18,3 +18,16 @@ def findRoot(f,a,b,epsilon):
             return findRoot(f,m,b,epsilon)    
     else:
         return 'Error!'
+
+
+def findAllRoots(f, a, b, epsilon):
+    m = (a + b)/2
+    if abs(b - a) <= epsilon:
+        if f(a) == 0 or (f(a) < 0 and f(b) > 0) or (f(a) > 0 and f(b) < 0):
+            return [m]
+        else:
+            return []
+    elif f(a) == 0:
+        return [a] + findAllRoots(f, a + epsilon, b, epsilon)
+    else:
+        return findAllRoots(f, a, m, epsilon) + findAllRoots(f, m, b, epsilon)
